f91168a887d8979471842037f1275dbc8914501f
Jonas Bernoulli <jonas@bernoul.li>
2020-06-30 17:57:23 +0200

    Mention another activation method
    Closes #4.

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ and arranges for them to be used in `outline-minor-mode`.
   :config (add-hook 'outline-minor-mode-hook
                     'outline-minor-faces-add-font-lock-keywords))
 ```
+
+If you want to only enable these faces in certain major-modes,
+then add this function to their hooks instead of to the above
+hook.
--- outline-minor-faces.el
+++ outline-minor-faces.el
@@ -33,11 +33,15 @@
 ;; and arranges for them to be used in `outline-minor-mode'.
 
 ;; Usage:
-;;
+
 ;;   (use-package outline-minor-faces
 ;;     :after outline
 ;;     :config (add-hook 'outline-minor-mode-hook
 ;;                       'outline-minor-faces-add-font-lock-keywords))
+
+;; If you want to only enable these faces in certain major-modes,
+;; then add this function to their hooks instead of to the above
+;; hook.
 
 ;;; Code:
 
