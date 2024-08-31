4628613f3570b865b2c22b750ebd41443c1848c2
Jonas Bernoulli <jonas@bernoul.li>
2024-09-01 00:09:12 +0200

    Release version 1.1.0

--- outline-minor-faces.el
+++ outline-minor-faces.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/outline-minor-faces
 ;; Keywords: faces outlines
 
-;; Package-Version: 1.0.2
+;; Package-Version: 1.1.0
 ;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
